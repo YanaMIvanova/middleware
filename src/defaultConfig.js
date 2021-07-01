c1441e21fc40297aa65e42455a9bce0d3610781d
Daniel Rodriguez <maieutiquer@gmail.com>
2021-07-01 23:37:31 +0300

    handle URLs

---
+++ src/defaultConfig.js
@@ -0,0 +1,2 @@
+export const apiUrl = 'http://localhost/apiv2'
+export const apiToken = 'b10e246fa52ca8ce9e68245026e7ebd3a14'
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import fetch from 'node-fetch'
 import cors from 'cors'
+import { apiToken, apiUrl } from './defaultConfig'
 
 const port = 4000
 
@@ -9,11 +10,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const getURL = (baseURL) => `${baseURL}?apitoken=b10e246fa52ca8ce9e68245026e7ebd3a14`
-
 app.get('/organizations', async (req, res) => {
     try {
-        const response = await fetch(getURL('http://localhost/apiv2/organization/list/recursive?page=0&limit=30'))
+        const url = new URL('organization/list/recursive', apiUrl)
+        const params = {
+            page: 0,
+            limit: 30,
+            apitoken: apiToken,
+        }
+
+        url.search = new URLSearchParams(params)
+
+        const response = await fetch(url)
         const organizations = await response.json()
 
         res.json(organizations)
