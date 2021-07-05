acc8ff72411d77d7b85d658fa8f56ad00bc7f4e7
Yana Ivanova <yana@plam.ch>
2021-07-05 16:00:40 +0300

    refactor endpoints and add another one for all courses

--- src/endpoints.js
+++ src/endpoints.js
@@ -2,15 +2,27 @@ import { callApi } from './callApi'
 import { checkIsOrgDescendantOfParent } from './utils'
 
 export const generateEndpoints = ({ app }) => {
+    // organizations START
     app.get('/organizationsOfVaud', async (_req, res) => {
         try {
             const organizations = await callApi({
                 path: 'organization',
                 params: { page: 0, limit: 10000 }, // TODO unlimited? Just remove params?
-                predicate: ({ parent }) => parent?.name === 'vd-test-filter',
             })
 
-            res.json(organizations)
+            res.json(organizations.filter(({ parent }) => parent?.name === 'vd-test-filter'))
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    // organizations END
+
+    // users START
+    app.get('/allUsers', async (_req, res) => {
+        try {
+            const users = await callApi({ path: 'user' })
+
+            res.json(users)
         } catch (error) {
             console.error(error)
         }
@@ -18,13 +30,14 @@ export const generateEndpoints = ({ app }) => {
 
     app.get('/usersInVaud', async (_req, res) => {
         try {
-            const users = await callApi({
-                path: 'user',
-                predicate: ({ mainOrganization: { code, parent } }) =>
-                    code === 'vd-test-filter' || parent?.code === 'vd-test-filter',
-            })
+            const users = await callApi({ path: 'user' })
 
-            res.json(users)
+            const filteredUsers = users.filter(
+                ({ mainOrganization: { code, parent } }) =>
+                    code === 'vd-test-filter' || parent?.code === 'vd-test-filter'
+            )
+
+            res.json(filteredUsers)
         } catch (error) {
             console.error(error)
         }
@@ -46,4 +59,35 @@ export const generateEndpoints = ({ app }) => {
             console.error(error)
         }
     })
+    // users END
+
+    // courses START
+    app.get('/courses', async (_req, res) => {
+        try {
+            const courses = await callApi({ path: 'data_source/all_courses/home' })
+
+            res.json(courses ?? `No courses found`)
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+    // app.get('/courseSessionsWithLearners', async (_req, res) => {
+    //     try {
+    //         const courseSessions = await callApi({ path: 'cursus_session' })
+    //
+    //         const sessionsWithLearners = courseSessions.filter((session) => session.participants.learners > 0)
+    //
+    //         sessionsWithLearners.forEach(({ id: sessionId }) =>
+    //             (async () => {
+    //                 const learners = await callApi({ path: `cursus_session/${sessionId}/users/learner` })
+    //             })()
+    //         )
+    //
+    //         res.json(sessionsWithLearners ?? `No sessions with learners found`)
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // })
+    // courses END
 }
